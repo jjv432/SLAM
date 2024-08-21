d227bae93f90143bc1c96acc71ad3669eb49bca5
jjv432 <98296519+jjv432@users.noreply.github.com>
2024-08-21 16:37:07 -0400

    Uploaded from correct folder

--- example_slam.launch.py
+++ example_slam.launch.py
@@ -10,29 +10,36 @@ def generate_launch_description():
         plugin='nvidia::isaac_ros::visual_slam::VisualSlamNode',
         parameters=[{
             
-        'enable_image_denoising': False,
-        'enable_slam_visualization': False,
-           'rectified_images': True,
-           'enable_imu_fusion': False,
-           'image_jitter_threshold_ms': 40.00,
-           'base_frame': 'camera_link',
-           'enable_slam_visualization': True,
-           'enable_landmarks_view': False,
-           'enable_observations_view': False,
+        
+        'rectified_images': False,
 
-            'num_cameras': 2, # 2 cams within one stereo camera
-            'base_frame': 'camera_link',
-            'verbocity': 3,
+        'enable_imu_fusion': False,
+        'debug_imu_mode': False,
+        'enable_debug_mode': False,
 
-            'camera_optical_frames': [
-                'left_camera_link',
-                'right_camera_link',
-            ],
+        'enable_slam_visualization': True, # visualization may affects the performance
+        'enable_landmarks_view': True, # visualization may affects the performance
+        'enable_observations_view': False, # visualization may affects the performance
+
+        'enable_localization_n_mapping': False,
+
+        'image_jitter_threshold_ms': 40.00, # for 30 FPS
+        'sync_matching_threshold_ms': 10.0, # desync in ms between different cams
+
+        'num_cameras': 2, # 2 cams within one stereo camera
+        'base_frame': 'camera_link',
+        'verbocity': 3,
+
+
+            # 'camera_optical_frames': [
+            #     'left_camera_link',
+            #     'right_camera_link',
+            # ],
         }],
 
-        remappings=[('visual_slam/image_0', '/stereo/left/image_rect'),
+        remappings=[('visual_slam/image_0', '/stereo/left/image_mono'),
                     ('visual_slam/camera_info_0', '/stereo/left/camera_info'),
-                    ('visual_slam/image_1', '/stereo/right/image_rect'),
+                    ('visual_slam/image_1', '/stereo/right/image_mono'),
                     ('visual_slam/camera_info_1', '/stereo/right/camera_info')
             ]
     )
